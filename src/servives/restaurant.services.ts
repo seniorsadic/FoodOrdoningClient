929fa1f35fbd64aa91c79070ba13f7e50736edcd
Abdul Aziz <zizasenior@gmail.com>
2018-05-23 09:22:05 +0000

    Création du service
    On crée les méthodes qui attaque notre web service

---
+++ src/servives/restaurant.services.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {Http,Headers} from '@angular/http';
+import {Restaurant} from "../model/model.restaurant";
+
+@Injectable()
+export class RestaurantService {
+
+  constructor(public http: Http) {}
+
+  getRestaurants() {
+    return this.http.get("http://localhost/myappli/web/app_dev.php/restaurants");
+  }
+
+  getRestaurantById(id:number) {
+    return this.http.get("http://localhost/myappli/web/app_dev.php/restaurants/"+id);
+  }
+
+  saveRestaurant(restaurant:Restaurant) {
+    return this.http.post("http://localhost/myappli/web/app_dev.php/restaurants",restaurant);
+  }
+
+  updateRestaurant(restaurant:Restaurant) {
+    return this.http.put("http://localhost/myappli/web/app_dev.php/restaurants/"+restaurant.id_resto,restaurant);
+  }
+
+  deleteRestaurant(id:number) {
+    return this.http.delete("http://localhost/myappli/web/app_dev.php/restaurants/"+id);
+  }
+
+}
