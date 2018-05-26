8460714c4e1bb2e4137e3c0dd46f61cfaa30cac1
topchico <33789800+topchico@users.noreply.github.com>
2018-05-26 12:47:53 +0000

    commit 2

--- src/servives/categorie.services.ts
+++ src/servives/categorie.services.ts
@@ -4,27 +4,27 @@ import {Categorie} from "../model/model.categorie";
 
 @Injectable()
 export class CategorieService {
-
+   URl2:string="http://www.sadicomputing.com/FoodOrdering/web/";
   constructor(public http: Http) {}
 
   getCategories() {
-    return this.http.get("http://www.sadicomputing.com/FoodOrdering/web/categories");
+    return this.http.get(this.URl2+"categories");
   }
 
   getCategorieById(id:number) {
-    return this.http.get("http://www.sadicomputing.com/FoodOrdering/web/categories/"+id);
+    return this.http.get(this.URl2+"categories/"+id);
   }
 
   saveCategorie(categorie:Categorie) {
-    return this.http.post("http://www.sadicomputing.com/FoodOrdering/web/categories",categorie);
+    return this.http.post(this.URl2+"categories",categorie);
   }
 
   updateCategorie(categorie:Categorie) {
-    return this.http.put("http://www.sadicomputing.com/FoodOrdering/web/categories/"+categorie.id_categorie,categorie);
+    return this.http.put(this.URl2+"categories/"+categorie.id_categorie,categorie);
   }
 
   deleteCategorie(id:number) {
-    return this.http.delete("http://www.sadicomputing.com/FoodOrdering/web/categories/"+id);
+    return this.http.delete(this.URl2+"categories/"+id);
   }
 
 }
