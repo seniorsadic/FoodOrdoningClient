3433abebd3a204d5679ff50a70100991f4a7991a
Abdul Aziz <zizasenior@gmail.com>
2018-05-26 12:58:31 +0000

    Test1

--- src/servives/categorie.services.ts
+++ src/servives/categorie.services.ts
@@ -4,21 +4,13 @@ import {Categorie} from "../model/model.categorie";
 
 @Injectable()
 export class CategorieService {
-<<<<<<< HEAD
-   URl2:string="http://www.sadicomputing.com/FoodOrdering/web/";
+
+  URl2:string="http://www.sadicomputing.com/FoodOrdering/web/"
+
   constructor(public http: Http) {}
 
   getCategories() {
     return this.http.get(this.URl2+"categories");
-=======
-
-  URl:string="http://www.sadicomputing.com/FoodOrdering/web/"
-
-  constructor(public http: Http) {}
-
-  getCategories() {
-    return this.http.get(this.URl+"categories");
->>>>>>> 579f1aed972c5e6c9af850f923eebf4ff9a2ca48
   }
 
   getCategorieById(id:number) {
