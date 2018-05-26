6c1a2ddccf65b9ef40a7b2a19f61a785fa500e08
topchico <33789800+topchico@users.noreply.github.com>
2018-05-26 16:41:23 +0000

    last modif

--- src/servives/categorie.services.ts
+++ src/servives/categorie.services.ts
@@ -5,40 +5,33 @@ import {Categorie} from "../model/model.categorie";
 @Injectable()
 export class CategorieService {
 
-<<<<<<< HEAD
-  URl:string="http://www.sadicomputing.com/FoodOrdering/web/"
-=======
+
   URl2:string="http://www.sadicomputing.com/FoodOrdering/web/"
->>>>>>> ccedcbd3890e28bbc6343c98f77474e51a810c33
+
 
   constructor(public http: Http) {}
 
   getCategories() {
-<<<<<<< HEAD
-    return this.http.get(this.URl+"categories");
-=======
     return this.http.get(this.URl2+"categories");
->>>>>>> ccedcbd3890e28bbc6343c98f77474e51a810c33
+
   }
 
   getCategorieById(id:number) {
-    return this.http.get(this.URl+"categories/"+id);
+    return this.http.get(this.URl2+"categories/"+id);
   }
 
   saveCategorie(categorie:Categorie) {
-    return this.http.post(this.URl+"categories",categorie);
+    return this.http.post(this.URl2+"categories",categorie);
   }
 
   updateCategorie(categorie:Categorie) {
-    return this.http.put(this.URl+"categories/"+categorie.id_categorie,categorie);
+    return this.http.put(this.URl2+"categories/"+categorie.id_categorie,categorie);
   }
 
   deleteCategorie(id:number) {
-<<<<<<< HEAD
-    return this.http.delete(this.URl+"categories/"+id);
-=======
+
     return this.http.delete(this.URl2+"categories/"+id); 
->>>>>>> ccedcbd3890e28bbc6343c98f77474e51a810c33
+
   }
 
 }
