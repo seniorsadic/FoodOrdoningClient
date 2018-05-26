41c57c652cbfbf05d3e0e4f2ee27743778146d48
Abdul Aziz <zizasenior@gmail.com>
2018-05-26 12:32:09 +0000

    Modication URL 2

--- src/servives/article.services.ts
+++ src/servives/article.services.ts
@@ -5,26 +5,28 @@ import {Article} from "../model/model.article";
 @Injectable()
 export class ArticleService {
 
+  URl:string="http://www.sadicomputing.com/FoodOrdering/web/";
+
   constructor(public http: Http) {}
 
   getArticles() {
-    return this.http.get("http://www.sadicomputing.com/FoodOrdering/web/articles");
+    return this.http.get(this.URl+"articles");
   }
 
   getArticleById(id:number) {
-    return this.http.get("http://www.sadicomputing.com/FoodOrdering/web/articles/"+id);
+    return this.http.get(this.URl+"articles/"+id);
   }
 
   saveArticle(article:Article) {
-    return this.http.post("http://www.sadicomputing.com/FoodOrdering/web/articles",article);
+    return this.http.post(this.URl+"articles",article);
   }
 
   updateArticle(article:Article) {
-    return this.http.put("http://www.sadicomputing.com/FoodOrdering/web/articles/"+article.id_article,article);
+    return this.http.put(this.URl+"articles/"+article.id_article,article);
   }
 
   deleteArticle(id:number) {
-    return this.http.delete("http://www.sadicomputing.com/FoodOrdering/web/articles/"+id);
+    return this.http.delete(this.URl+"articles/"+id);
   }
 
 }
