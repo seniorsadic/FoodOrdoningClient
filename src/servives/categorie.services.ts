27c310e900c892b31ff5a971ee8058511e87bdd6
NabouNa <NabouNa@github.com>
2018-05-26 15:39:50 +0000

    URL

--- src/servives/categorie.services.ts
+++ src/servives/categorie.services.ts
@@ -4,13 +4,6 @@ import {Categorie} from "../model/model.categorie";
 
 @Injectable()
 export class CategorieService {
-<<<<<<< HEAD
-   URl2:string="http://www.sadicomputing.com/FoodOrdering/web/";
-  constructor(public http: Http) {}
-
-  getCategories() {
-    return this.http.get(this.URl2+"categories");
-=======
 
   URl:string="http://www.sadicomputing.com/FoodOrdering/web/"
 
@@ -18,23 +11,22 @@ export class CategorieService {
 
   getCategories() {
     return this.http.get(this.URl+"categories");
->>>>>>> 579f1aed972c5e6c9af850f923eebf4ff9a2ca48
   }
 
   getCategorieById(id:number) {
-    return this.http.get(this.URl2+"categories/"+id);
+    return this.http.get(this.URl+"categories/"+id);
   }
 
   saveCategorie(categorie:Categorie) {
-    return this.http.post(this.URl2+"categories",categorie);
+    return this.http.post(this.URl+"categories",categorie);
   }
 
   updateCategorie(categorie:Categorie) {
-    return this.http.put(this.URl2+"categories/"+categorie.id_categorie,categorie);
+    return this.http.put(this.URl+"categories/"+categorie.id_categorie,categorie);
   }
 
   deleteCategorie(id:number) {
-    return this.http.delete(this.URl2+"categories/"+id);
+    return this.http.delete(this.URl+"categories/"+id);
   }
 
 }
